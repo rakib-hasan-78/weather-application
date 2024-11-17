dc5f37099650dba579e20e2edae05411fd67a139
rakib-hasan-78 <rakibwebdev78@gmail.com>
2024-11-17 18:02:30 +0600

    first action added

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ import './src/image/rainy.jpg';
 import './src/image/autumn.jpg';
 import './src/image/spring.jpg';
 
+const searchContainer = document.getElementById('search-container');
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', (e)=>{      
+
+    if (searchContainer.classList.contains('w-full')) {
+        searchContainer.classList.remove('w-full');
+        searchContainer.classList.add('w-16', 'h-16');
+    } else {
+        searchContainer.classList.remove('w-16','h-16');
+        searchContainer.classList.add('w-full');
+    
+    }
+
+});
